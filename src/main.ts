c4168f8029c44f411bcbfef7ad4f8e856619d439
Hnzsama <hnzsama@gmail.com>
2026-08-22 22:40:53 +0700

    feat: enable trust proxy for Cloudflare Tunnel support

--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,14 @@
 process.env.TZ = 'Asia/Jakarta';
 
 import { NestFactory } from '@nestjs/core';
+import { NestExpressApplication } from '@nestjs/platform-express';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { json, urlencoded } from 'express';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  app.set('trust proxy', 1);
 
   app.use(json({ limit: '10mb' }));
   app.use(urlencoded({ limit: '10mb', extended: true }));
